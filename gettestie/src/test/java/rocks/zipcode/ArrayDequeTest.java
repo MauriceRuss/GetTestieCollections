b2ec577b59a7ad74f5cb03ebfe01105df14f1afd
Mo Russ <mauriceruss302@gmail.com>
2020-03-06 18:45:52 -0500

    testArrayDeque passed

--- gettestie/src/test/java/rocks/zipcode/ArrayDequeTest.java
+++ gettestie/src/test/java/rocks/zipcode/ArrayDequeTest.java
@@ -1,4 +1,36 @@
 package rocks.zipcode;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+
+import static org.junit.Assert.assertEquals;
+
 public class ArrayDequeTest {
+    private static String TEST_STRING1 = "Test_String1";
+    private static String TEST_STRING2 = "Test_String2";
+    private ArrayDeque<String> array;
+
+    @Before
+    public void setUp() throws Exception {
+        array = new ArrayDeque<>();
+        array.add(TEST_STRING1);
+        array.add(TEST_STRING2);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testArrayDeque() {
+        assertEquals(array.size(),2);
+        array.remove(TEST_STRING1);
+        assertEquals(array.size(),1);
+        Assert.assertFalse(array.isEmpty());
+    }
+
 }
--- gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
+++ gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
@@ -1,4 +1,38 @@
 package rocks.zipcode;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+
 public class LinkedListTest {
+
+    private static String TEST_STRING1 = "Test_String1";
+    private static String TEST_STRING2 = "Test_String2";
+    private LinkedList<String> array;
+
+    @Before
+    public void setUp() throws Exception {
+        LinkedList<String> array = new LinkedList<String>();
+        array.add(TEST_STRING1);
+        array.add(TEST_STRING2);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+//    @Test
+//    public void testLinkedList() {
+//        //assertEquals(array.getLast(),TEST_STRING2);
+//        assertEquals(array.getFirst(),TEST_STRING1);
+//        array.remove(TEST_STRING1);
+//        assertEquals(array.size(),1);
+//    }
+
+
 }
